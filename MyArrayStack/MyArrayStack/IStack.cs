3e4443d93ce60938bb65b9cb8676ef0f7bda2ab3
Irishka <IrinaLaskova@mail.ru>
2017-09-23 11:35:41 +0300

    Создан и реализован интерфейс IStack

---
+++ MyArrayStack/MyArrayStack/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArrayStack
+{
+        interface IStack<T>
+        {
+            void Push(T element);
+            T Pop();
+            T Peek();
+            bool Contains(T a);
+
+        }
+    }
