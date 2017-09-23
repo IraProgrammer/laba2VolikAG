4fd45c86ab31640fce88ba47fe42fd3c8aff8f32
Irishka <IrinaLaskova@mail.ru>
2017-09-23 11:41:57 +0300

    Изменён класс Programm

--- MyArrayStack/MyArrayStack/Program.cs
+++ MyArrayStack/MyArrayStack/Program.cs
@@ -10,6 +10,29 @@ namespace MyArrayStack
     {
         static void Main(string[] args)
         {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(7);
+            stack.Push(9);
+            stack.Push(2);
+            stack.Push(8);
+            stack.Push(7);
+            stack.Push(15);
+            stack.Push(7);
+
+            int a = 7;
+            if (stack.Contains(a) == true) Console.WriteLine("Элемент " + a + " найден");
+
+            Console.WriteLine("Размер стека равен " + stack.Count);
+            Console.WriteLine("Верхушка стека - это " + stack.Peek());
+            int count = stack.Count;
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine("Элемент номер " + stack.Count + " равен " + stack.Pop());
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
